5f9f35d785921f3d648ebcef5f7ba52b178e13f9
anhv7878-ctrl <anhv7878@gmail.com>
2025-10-23 11:13:19 +0700

    Create AppointmentService.java

---
+++ app/src/main/java/com/project/back_end/services/AppointmentService.java
@@ -0,0 +1,12 @@
+package com.smartclinic.app.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppointmentService {
+
+    // Cần thêm các phương thức logic nghiệp vụ (business logic)
+    public void bookAppointment(...) {
+        // Logic đặt lịch hẹn
+    }
+}
