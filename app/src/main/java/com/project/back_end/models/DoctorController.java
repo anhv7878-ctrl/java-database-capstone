e46c6699ba59c3bf4611ea81c5d95d46ffa605aa
anhv7878-ctrl <anhv7878@gmail.com>
2025-10-23 11:10:16 +0700

    Create DoctorController.java

---
+++ app/src/main/java/com/project/back_end/models/DoctorController.java
@@ -0,0 +1,10 @@
+package com.smartclinic.app.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/doctors")
+public class DoctorController {
+    // Cần thêm các phương thức API (GET, POST, PUT, DELETE)
+}
