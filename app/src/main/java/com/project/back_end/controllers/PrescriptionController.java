5d655c7b617c2c697eb89f94bcea8e1fb9ecded4
anhv7878-ctrl <anhv7878@gmail.com>
2025-10-23 11:14:46 +0700

    Create PrescriptionController.java

---
+++ app/src/main/java/com/project/back_end/controllers/PrescriptionController.java
@@ -0,0 +1,10 @@
+package com.smartclinic.app.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/prescriptions")
+public class PrescriptionController {
+    // Cần thêm các phương thức API (GET, POST)
+}
